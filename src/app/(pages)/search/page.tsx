ff687670814be0efb69221394920125728a71198
d-lite <olabemiwodaniel@gmail.com>
2023-11-03 01:03:52 +0100

    chore: add sidebar demo WIP

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -67,6 +67,7 @@ const SearchPage = () => {
                 </svg>
               </button>
             </div>
+            
           </div>
         </div>
       </div>
--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -1,20 +1,93 @@
+'use client'
+
 import React from 'react';
+import Select, { StylesConfig } from 'react-select';
 
 interface Props {
     name: string;
 }
+interface ColourOption {
+    readonly value: string;
+    readonly label: string;
+    readonly color: string;
+    readonly isFixed?: boolean;
+    readonly isDisabled?: boolean;
+}
+
+const colourOptions: readonly ColourOption[] = [
+    { value: 'ocean', label: 'Ocean', color: '#00B8D9', isFixed: true },
+    { value: 'blue', label: 'Blue', color: '#0052CC', isDisabled: true },
+    { value: 'purple', label: 'Purple', color: '#5243AA' },
+    { value: 'red', label: 'Red', color: '#FF5630', isFixed: true },
+    { value: 'orange', label: 'Orange', color: '#FF8B00' },
+    { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+    { value: 'green', label: 'Green', color: '#36B37E' },
+    { value: 'forest', label: 'Forest', color: '#00875A' },
+    { value: 'slate', label: 'Slate', color: '#253858' },
+    { value: 'silver', label: 'Silver', color: '#666666' },
+];
+
 
 const SearchFilter = () => {
     return (
-        <div className='w-full'>
-            <div className="flex flex-col my-8 gap-[10px] px-2">
+        <div className='w-full py-8 flex flex-col items-center'>
+            <div className="flex w-full flex-col my-2 gap-[10px] px-4">
                 <p>Country</p>
-                <select className="row text-[1rem] px-8 py-2 cursor-pointer text-gray-500 bg-white border border-gray-400 rounded-md shadow-sm outline-none appearance-none focus:border-black">
-                    <option value="-can_apply,rank">Featured</option>
-                    <option value="-can_apply,discounted_price">Low to High</option>
-                    <option value="-can_apply,-discounted_price">High to Low</option>
-                </select>
+
+                <Select
+                    placeholder='Select country'
+                    options={colourOptions}
+                />
             </div>
+
+            <div className="flex flex-col my-2 gap-[10px] px-4 w-full">
+                <p>University Type</p>
+
+                <Select
+                    placeholder='Select University Type'
+                    options={colourOptions}
+                />
+            </div>
+
+            <div className="flex flex-col my-2 gap-[10px] px-4 w-full">
+                <p>Grade Type</p>
+
+                <Select
+                    placeholder='Select Grade Type'
+                    options={colourOptions}
+                />
+            </div>
+
+            <div className="flex flex-col my-2 gap-[10px] px-4 w-full">
+                <p>Education Type</p>
+
+                <Select
+                    placeholder='Select Education Type'
+                    options={colourOptions}
+                />
+            </div>
+
+            <div className="flex flex-col my-2 gap-[10px] px-4 w-full">
+                <p>Campus Type</p>
+
+                <Select
+                    placeholder='Select Campus Type'
+                    options={colourOptions}
+                />
+            </div>
+
+            <div className="flex flex-col my-2 gap-[10px] px-2">
+                <p>Education Language</p>
+
+                <Select
+                    placeholder='Select Education Language'
+                    options={colourOptions}
+                />
+            </div>
+
+            <button className='bg-[#0B69FF]  w-[80%] text-white rounded-[15px] py-2 px-8 mt-4'>
+                CLEAR FILTERS
+            </button>
         </div>
     );
 };
